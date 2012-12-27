29f1d19df856b99e7161516519187fa1505fa732
gdusbabek <gdusbabek@gmail.com>
2012-12-26 23:33:16 -0600

    global rdio object
    because that seems to be what oauth requires.

--- site/lib/rdio.js
+++ site/lib/rdio.js
@@ -1,9 +1,11 @@
 var Rdio = require('rdio-node').Rdio;
 
-var auth = {
+var params = {
     consumerKey: process.env.RDIO_KEY,
     consumerSecret: process.env.RDIO_SECRET
 };
+
+var rdio = new Rdio(params);
 
 function dedupeSongs(songs) {
     var newSongs = [];
@@ -20,33 +22,92 @@ function dedupeSongs(songs) {
 
 // expects?
 exports.search = function(q, callback) {
-    var rdio = new Rdio(auth),
-        rdioQuery = {
+    var rdioQuery = {
             query: q,
             types: 'Song'
         };
     rdio.makeRequest('search', rdioQuery, function(err, results) {
-        var status = results.status,
-            track_count = results.result.track_count,
-            number_results = results.result.number_results,
-            songs = results.result.results,
-            songsBack = [];
-        songs.forEach(function(song) {
-            songsBack[songsBack.length] = {artist: song.artist, title: song.name};
-        });
-        songsBack = dedupeSongs(songsBack);
-        callback(err, songsBack);
+        if (err) {
+            callback(err, null);
+        } else {
+            var status = results.status,
+                track_count = results.result.track_count,
+                number_results = results.result.number_results,
+                songs = results.result.results,
+                songsBack = [];
+            songs.forEach(function(song) {
+                songsBack[songsBack.length] = {artist: song.artist, title: song.name};
+            });
+            songsBack = dedupeSongs(songsBack);
+            callback(err, songsBack);
+        }
     });
 }
 
-// exports.search('Friday I\'m in Love The Cure', function(err, results) {
-//     if (err) {
-//         console.log('ouch');
-//         process.exit(0);
-//     } else {
-//         results.forEach(function(song) {
-//             console.log(song.artist + ': ' + song.name);
-//         });
-//         process.exit(0);
-//     }
+function Store(data) {
+    this.data = data || {}
+}
+
+Store.parse = function(strInput) {
+    var data = strInput ? JSON.parse(strInput) : null;
+    return new Store(data);
+}
+
+Store.prototype.get = function(key) {
+    console.log('GETTT');
+    return this.data[key];
+}
+
+Store.prototype.set = function(key, val) {
+    console.log('SETTTT');
+    this.data[key] = val;
+}
+
+Store.prototype.remove = function(key) {
+    console.log('REMOVEEEEE');
+    delete this.data[key];
+}
+
+Store.prototype.removeAll = function() {
+    this.data = {};
+}
+
+Store.prototype.stringify = function() {
+    console.log('STRINGIFYYYYY');
+    return JSON.stringify(this.data);
+}
+
+// var dataStore = new Store();
+// params.dataStore = dataStore;
+// var r = rdio;
+
+// r.makeRequest('search', {query: 'Archers of Loaf', types: 'Artist'}, function() {
+//   console.log(arguments);
 // });
+
+// // Make an authenticated request (with OAuth flow)
+// r.beginAuthentication(function(error, loginUrl) {
+//     if (error) {
+//       console.log(error);
+//       return;
+//     }
+
+//     var stdin = process.stdin, stdout = process.stdout;
+
+//     stdin.resume();
+//     stdout.write('visit: ' + loginUrl + '\nEnter your pin: ');
+
+//     stdin.once('data', function(data) {
+//       data = data.toString().trim();
+//       r.completeAuthentication(data, function() {
+
+//         // Notice how unlike the call to 'search' above,
+//         // 'getPlaylists' doesn't need any parameters.
+//         r.makeRequest('getPlaylists', function() {
+//           console.log(arguments[1]);
+//           process.exit();
+//         });
+//       });
+//     });
+//   }
+// );
