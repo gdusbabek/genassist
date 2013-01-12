2f361dc605f31e2ae62f77d8a526a50e88386661
gdusbabek <gdusbabek@gmail.com>
2013-01-12 06:26:01 -0600

    Invitation to link to last.fm in the footer.

--- site/public/js/footer.js
+++ site/public/js/footer.js
@@ -6,4 +6,13 @@ $(document).ready(function() {
     } else if ($('#hide_rdio_invitation_in_footer').length > 0) {
         $('#is_not_rdio').addClass('hide');
     }
+    
+    if ($.cookie('lastLink')) {
+        $('#is_not_last').addClass('hide');
+    } else if ($('#hide_last_invitation_in_footer').length > 0) {
+        $('#is_not_last').addClass('hide');
+    } else {
+        // todo: decorate the link with a callback to this page.
+        
+    }
 });
